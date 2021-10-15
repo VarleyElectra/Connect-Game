0ef1f23aea4678b05b052fc9a14488427e2beb15
Alex Gerasimov <alexandr041299@gmail.com>
2021-10-15 16:26:06 +0500

    Реализована механика игры

--- src/GameField.js
+++ src/GameField.js
@@ -1,7 +1,8 @@
 import * as PIXI from 'pixi.js';
 import {Blob} from './Blob.js';
 import StraightLine from "./StraightLine";
-import {BLOB_COLORS_DIGITS} from "./constants"
+import {BLOB_COLORS_DIGITS, GAME_MATRIX_SIZE, BLOB_COLORS} from "./constants";
+import {Utils} from "./Utils";
 
 export default class GameField extends PIXI.Container {
     constructor(spacing, blobDimensionCount) {
@@ -35,21 +36,83 @@ export default class GameField extends PIXI.Container {
         this.isDown = false;
         this.lines.forEach(line => line.destroy());
         this.lines = [];
+        const deletedBlobsByColumn = [];
+        for (let i = 0; i < GAME_MATRIX_SIZE; i++) {
+            deletedBlobsByColumn.push([]);
+        }
+        const blobsForMove = [];
+
         if (this.blobChain.length >= 2) {
             let blobColor = this.blobChain[0].color;
+
             this.blobChain.forEach(blob => {
-                console.log(blob.position);
-                blob.destroy();
+                let curBlobXNum = this.getBlobGameFieldPosition(blob.x);
+
+                deletedBlobsByColumn[curBlobXNum].push(blob);
+
                 if (blobColor === this.task1Color) {
                     game.dataStorage.currentTaskCount1 += 1;
                 }
                 if (blobColor === this.task2Color) {
                     game.dataStorage.currentTaskCount2 += 1;
                 }
-            });
-        }
 
-        this.blobChain = [];
+            });
+
+            deletedBlobsByColumn.forEach(column => {
+                let yLowest = GAME_MATRIX_SIZE - 1;
+                let curBlobXNum;
+                column.forEach(blob => {
+                    if (blob) {
+                        let curBlobYNum = this.getBlobGameFieldPosition(blob.y);
+                        if (curBlobYNum < yLowest) {
+                            yLowest = curBlobYNum;
+                        }
+                    }
+                })
+
+                if (column.length) {
+                    curBlobXNum = this.getBlobGameFieldPosition(column[0].x)
+
+                    for (let i = 0; i < yLowest; i++) {
+                        blobsForMove.push(game.level.gameField.children[curBlobXNum].children[i]);
+                    }
+                }
+            })
+
+            this.blobChain.forEach(blob => {
+                blob.visible = false;
+            })
+
+            Promise.all(blobsForMove.map(blob => {
+                const curBlobXNum = this.getBlobGameFieldPosition(blob.x);
+                let from = {x: blob.x, y: blob.y};
+                const to = {x: blob.x, y: blob.y + deletedBlobsByColumn[curBlobXNum].length * this.spacing};
+                return this.moveBlobs(blob, from, to);
+            })).then(() => {
+                deletedBlobsByColumn.forEach(column => {
+                    if (column.length) {
+                        let xGameFieldPosition = this.getBlobGameFieldPosition(column[0].x);
+                        for (let i = 0; i < column.length; i++) {
+                            let randomNum = Utils.getRandomNumber(0, Object.values(BLOB_COLORS).length - 1);
+                            let newBlobColor = Utils.getBlobColor(randomNum);
+                            let newBlob = new Blob(newBlobColor);
+                            newBlob.position.set(column[0].x, 20 + i * this.spacing);
+                            game.level.gameField.children[xGameFieldPosition].addChildAt(newBlob, i);
+                        }
+                    }
+                })
+
+                this.blobChain.forEach(blob => {
+                    blob.destroy();
+                })
+                this.blobChain = [];
+
+            }).catch(() => {
+                console.log("err")
+            })
+            game.dataStorage.currentStep -= 1;
+        }
         this.currentBlob = null;
     }
 
@@ -68,6 +131,10 @@ export default class GameField extends PIXI.Container {
 
     get movingLine() {
         return this.lines[this.lines.length - 1];
+    }
+
+    getBlobGameFieldPosition(coordinate) {
+        return (coordinate - 20) / this.spacing;
     }
 
     move(e) {
@@ -99,17 +166,36 @@ export default class GameField extends PIXI.Container {
         );
     }
 
+    async moveBlobs(target, from, to) {
+        return new Promise (resolve => {
+            const dx  = (to.x - from.x) / 20;
+            const dy  = (to.y - from.y) / 20;
+
+            let i = 0;
+            const intervalId = setInterval(() => {
+                if (i === 21) {
+                    clearInterval(intervalId);
+                    resolve();
+                    return;
+                }
+                target.position.set(from.x + dx * i, from.y + dy * i);
+                i++;
+            }, 25);
+        })
+    }
+
     drawField() {
         for (let i = 0; i < this.blobDimensionCount; i++) {
             const x = this.spacing * i + 20;
-
+            const column = new PIXI.Container();
             for (let j = 0; j < this.blobDimensionCount; j++) {
                 let blob =  new Blob(game.dataStorage.blobColorsMatrix[i][j]);
                 const y = this.spacing * j + 20;
 
                 blob.position.set(x, y);
-                this.addChild(blob);
+                column.addChild(blob);
             }
+            this.addChild(column);
         }
     }
 }
